b9febbcbc824b5eef9242cddcb116802ea6063e8
João Pesce <jpesce@gmail.com>
2020-09-02 17:51:30 -0300

    Add italics support to iterm

---
+++ iterm2/enable-italics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ---- Override the local entry for xterm-256color
+# https://eddieantonio.ca/blog/2015/04/16/iterm-italics/
+# Only affects your machine so remote machines won’t display italics. WARNING:
+# you might have a problem sshing into your Mac.
+infocmp xterm-256color > /tmp/xterm-256color.terminfo
+printf '\tsitm=\\E[3m, ritm=\\E[23m,\n' >> /tmp/xterm-256color.terminfo
+tic /tmp/xterm-256color.terminfo
+
+# Or if you prefere to create a new terminfo and have an ssh alias:
+# https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
