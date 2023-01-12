aa342653e3388d1b1a7ee19f28c37cc86777821d
João Pesce <jpesce@gmail.com>
2023-01-12 12:09:39 -0300

    edit(tmux): use 30 chars in branch name display

--- tmux/status/status_right.sh
+++ tmux/status/status_right.sh
@@ -5,6 +5,6 @@ branch=$(git -C $1 rev-parse --abbrev-ref HEAD 2>/dev/null)
 
 color="#[fg=white]"
 
-# Add space to text so it has always 20 characters
+# Add space to text so it has always 30 characters
 # See https://unix.stackexchange.com/a/594880 for the arcane
-printf '%*s%s%s' "$((20-${#text}))" "" "$color" "$text"
+printf '%*s%s%s' "$((30-${#text}))" "" "$color" "$text"
