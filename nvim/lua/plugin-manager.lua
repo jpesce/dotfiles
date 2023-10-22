7b4f669a89fe2df86c6df635562a81a5d1412e8e
João Pesce <jpesce@gmail.com>
2023-10-22 16:16:51 -0300

    edit(nvim): move plugin logic to correct file

--- nvim/lua/plugin-manager.lua
+++ nvim/lua/plugin-manager.lua
@@ -1,3 +1,16 @@
+local ensure_packer_is_installed = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_just_installed = ensure_packer_is_installed()
+
 packer = require 'packer'
 local util = require 'packer.util'
 
@@ -6,3 +19,7 @@ packer.init({
 })
 
 packer.use 'wbthomason/packer.nvim'
+
+if packer_just_installed then
+  require('packer').sync()
+end
