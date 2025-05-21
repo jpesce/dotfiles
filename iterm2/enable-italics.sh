438ba451771427607458e16872b80e3b75ce7359
João Pesce <jpesce@gmail.com>
2025-05-20 21:54:04 -0300

    iterm: tweak comment

--- iterm2/enable-italics.sh
+++ iterm2/enable-italics.sh
@@ -3,7 +3,7 @@
 # ---- Override the local entry for xterm-256color
 # https://eddieantonio.ca/blog/2015/04/16/iterm-italics/
 # Only affects local machine so remote machines still won’t be able to display italics.
-# WARN: might have a problem sshing into your Mac.
+# WARN: might have a problem when sshing into your mac.
 infocmp xterm-256color > /tmp/xterm-256color.terminfo
 # printf '\tsitm=\\E[3m, ritm=\\E[23m,\n' >> /tmp/xterm-256color.terminfo
 tic /tmp/xterm-256color.terminfo
